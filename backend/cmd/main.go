bca3271682c8bc8a17881c36392eef3d01d0e2a4
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2025-10-16 00:02:23 -0400

    feat: set up docker

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Using the Config value, create the DynamoDB client
+	svc := dynamodb.NewFromConfig(cfg)
+	// TODO: just printing for now. 
+	fmt.Print(svc)
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
@@ -14,4 +30,5 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.Run()
 }
